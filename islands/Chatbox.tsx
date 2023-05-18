bbaf286ca76dbb5fecfb4b5b815b9841a3f4c29c
jainpranav1 <pranavji@tamu.edu>
2023-05-18 00:05:20 -0500

    made the chatbox more responsive

--- islands/Chatbox.tsx
+++ islands/Chatbox.tsx
@@ -8,7 +8,7 @@ interface CounterProps {
 export default function Chatbox(props: CounterProps) {
   const [count, setCount] = useState(props.start);
   return (
-    <div class="w-full h-full max-w-screen-md border-4 border-indigo-500">
+    <div class="w-full lg:w-[70%] h-full border-4 border-indigo-500">
       <p>hello</p>
     </div>
   );
