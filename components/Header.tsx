dfb5fe8d38706106e106ef0b44386249f1f6615d
jainpranav1 <pranavji@tamu.edu>
2023-05-20 15:00:07 -0500

    added more navbar code

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,20 +17,20 @@ export default function Header({ active }: Props) {
   ];
 
   return (
-    <div class="bg-black w-full flex flex-row justify-center py-6">
-      <div class="max-w-screen-lg flex flex-row justify-between">
-        <div class="flex items-center flex-1">
-          <img src="/logo.png" width="40" height="40" />
-          <div class="text-white text-2xl  ml-1 font-bold">
+    <div class="bg-black w-full flex flex-row justify-center py-4">
+      <div class="max-w-screen-lg flex flex-row justify-center gap-28">
+        <div class="flex flex-row items-center flex-l">
+          <img src="/logo.png" width="30" height="30" />
+          <div class="text-white text-lg ml-1 font-bold whitespace-nowrap">
             Pranav's Site
           </div>
         </div>
-        <ul class="flex items-center gap-6">
+        <ul class="flex flex-row gap-4 items-center	">
           {menus.map((menu) => (
             <li>
               <a
                 href={menu.href}
-                class={"text-gray-500 hover:text-gray-700 py-1 border-gray-500" +
+                class={"whitespace-nowrap text-gray-500 hover:text-gray-700 py-1 border-gray-500" +
                   (menu.href === active ? " font-bold border-b-2" : "")}
               >
                 {menu.name}
