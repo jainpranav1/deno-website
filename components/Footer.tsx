148eb7b39f4a04a46cd17fb30d8c6fed4cd5a224
jainpranav1 <pranavji@tamu.edu>
2023-05-21 15:15:11 -0500

    added a footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import { useState } from "preact/hooks";
+
+export default function Footer() {
+  return (
+    <div class="bg-black w-full flex flex-row justify-center py-4 text-gray-400 fixed bottom-0">
+      © 2022 by Pranav Jain
+    </div>
+  );
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,15 @@
 import { Head } from "$fresh/runtime.ts";
 import Header from "../islands/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Pranav's Website</title>
+        <title>Pranav's Site</title>
       </Head>
       <Header active="/about_me"></Header>
+      <Footer></Footer>
     </>
   );
 }
