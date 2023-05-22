94e4e314ef2b5345003f9e85830ca6fbfce29e81
jainpranav1 <pranavji@tamu.edu>
2023-05-21 19:50:25 -0500

    got a basic homepage done

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,7 +8,20 @@ export default function Home() {
       <Head>
         <title>Pranav's Site</title>
       </Head>
-      <Header active="/about_me"></Header>
+      <Header active="/"></Header>
+      <div class="flex flex-row justify-evenly items-center">
+        <img src="/graduation_pic.png" class="rounded w-64" />
+        <div class="flex flex-col justify-center max-w-md">
+          <p class="text-4xl font-bold self-center mb-4">Pranav's Site</p>
+          <q class="self-center">
+            We live on this speck called Earth - think about what you might do,
+            today or tomorrow - and make the most of it.
+          </q>
+          <p class="self-end">
+            <b>- Neil deGrasse Tyson</b>
+          </p>
+        </div>
+      </div>
       <Footer></Footer>
     </>
   );
